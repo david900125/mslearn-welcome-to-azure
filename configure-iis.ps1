e4fd370e360da03f8cece185e94fcb179c3aba34
david900125 <63245937+david900125@users.noreply.github.com>
2020-11-11 15:16:01 +0800

    Update configure-iis.ps1

--- configure-iis.ps1
+++ configure-iis.ps1
@@ -6,7 +6,7 @@ Set-Content `
   -Path "C:\\inetpub\\wwwroot\\Default.htm" `
   -Value "<html><body><h2>Welcome to Azure! My name is s1110534033 $($env:computername).</h2></body></html>"
 
-<iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZWU3YTdhMjItMzRjOC00YjBiLThjNTMtNzMyZDIwNzBlMmJjIiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
+ -Value "<iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZWU3YTdhMjItMzRjOC00YjBiLThjNTMtNzMyZDIwNzBlMmJjIiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>"
 
-<iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMzc3MzlmYzgtODA3OS00NzY1LTk4NTEtMTI5MjIwN2M5NzQ4IiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
+ -Value "<iframe width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMzc3MzlmYzgtODA3OS00NzY1LTk4NTEtMTI5MjIwN2M5NzQ4IiwidCI6IjI4ZDBmYTc1LWY5ZjktNDAyNC05MzM3LTQ4NWQ0NmU3NTI1NyIsImMiOjEwfQ%3D%3D&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>"
 
